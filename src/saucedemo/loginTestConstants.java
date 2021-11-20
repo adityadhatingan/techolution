a19eec1adf4b11add2c62a7c25d733798782d4ae
adityadhatingan <59099880+adityadhatingan@users.noreply.github.com>
2021-11-20 20:49:30 +0530

    NameUpdate

--- src/saucedemo/loginTestConstants.java
+++ src/saucedemo/loginTestConstants.java
@@ -3,7 +3,7 @@ package saucedemo;
 import org.openqa.selenium.By;
 
 /** This interface file contains constants to test login feature of saucedemo. */
-public interface loginTestConstants {
+public interface LoginTestConstants {
   String SAUCEDEMO_WEBADDRESS = "https://www.saucedemo.com/";
   By USER_NAME = By.xpath("//input[@id='user-name']");
   By PASSWORD = By.xpath("//input[@id='password']");
